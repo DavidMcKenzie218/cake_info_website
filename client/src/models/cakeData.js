5aabdb45f703757e883ad017e0011814afc32c9a
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-02-13 15:39:52 +0000

    Added and tested a function that takes in the api data in the form of an array and adds all of the cakes individually to the cake data array

--- client/src/models/cakeData.js
+++ client/src/models/cakeData.js
@@ -13,6 +13,14 @@ CakeData.prototype = {
   //Adds data to the end of this.data
   addData: function(newData){
     this.data.push(newData)
+  },
+
+  //Takes in the api data in array from and adds all the cakes individually
+  addApiData: function(apiData){
+    let cakeList = apiData;
+    cakeList.forEach(function(cake){
+      this.addData(cake);
+    }.bind(this))
   }
 
 }
--- client/src/models/specs/cakeDataSpecs.js
+++ client/src/models/specs/cakeDataSpecs.js
@@ -4,6 +4,7 @@ const CakeData = require('../cakeData.js');
 describe("The Cake Data;", function(){
 
   let cakes;
+  let exampleData = [{name: "sponge"}, {name: "chocolate"}]
 
   before(function(){
     cakes = new CakeData();
@@ -20,6 +21,11 @@ describe("The Cake Data;", function(){
   it("should be able to add data", function(){
     cakes.addData("sponge")
     assert.deepEqual(["sponge"], cakes.data);
+  })
+
+  it("should be able to sort the incoming data into individule pieces of data", function(){
+    cakes.addApiData(exampleData);
+    assert.deepEqual([{name: "sponge"}, {name: "chocolate"}], cakes.data);
   })
 
 })
