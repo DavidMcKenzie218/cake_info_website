84eb985c6f71a4de4897ffa6bb8cc71de2cbad44
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-02-13 16:19:35 +0000

    Added a container for all of the front end react components

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,17 @@
+const React = require("react");
+const ReactDOM = require('react-dom');
 const ApiRequest = require('./models/dataHandler.js');
+
+const addDiv = function(parent){
+  let div = document.createElement('div');
+  div.id = 'react-container';
+  parent.appendChild(div);
+}
+
+const reactContainer = function(){
+  let body = document.querySelector('body');
+  addDiv(body);
+}
 
 window.onload = function(){
 
@@ -6,6 +19,11 @@ window.onload = function(){
 
   api.getAll("https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json");
 
-  console.log(api.Data)
+  reactContainer();
+
+  ReactDOM.render(
+    <h1>Hello World</h1>, 
+    document.querySelector('#react-container')
+  );
 
 }
